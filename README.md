3a9e0acda187dc2853339942b756be307b36a6e1
Chris Ian Fiel <ccfiel@gmail.com>
2016-09-07 16:02:58 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Amazon S3 Backup
+
+Using Amazon S3 for Backup Frappe and ERPNext
+
+#### License
+
+MIT
+
+
+#### How to Install
+
+
+1) Install boto and filechunkio packages
+   
+    a) source ./frappe-bench/env/bin/activate
+    b) pip install boto
+    c) pip install filechunkio
+
+2) bench get-app s3_backup https://github.com/ccfiel/s3_backup.git
+
+3) bench install-app s3_backup
+
+
+
+
---
+++ s3_backup/config/setup.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            "label": _("Integrations"),
+            "icon": "octicon octicon-cloud-upload",
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Amazon S3 Settings",
+                    "label": _("Amazon S3 Backup"),
+                    "description": _("Amazon S3 Backup for Frappe and ERPNext"),
+                    "hide_count": True
+                }
+            ]
+        }
+    ]
