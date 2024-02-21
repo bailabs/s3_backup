5665b0c2d9ed19d4cd515599ab1e0b95a7aee04a
dexluther <dexluther@wela.online>
2024-02-21 08:54:49 +0800

    added init.py

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
+# imports - standard imports
 from setuptools import setup, find_packages
+import re
+import ast
 import os
 
-version = '0.0.1'
+# get version from __version__ variable in frappe/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+with open('s3_backup/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
 
 setup(
     name='s3_backup',
@@ -15,3 +27,5 @@ setup(
     include_package_data=True,
     install_requires=("frappe",),
 )
+
+
